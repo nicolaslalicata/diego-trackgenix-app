da601ab4585e6e113cce075310a937503f914b18
Alvaro-Guardia <alvaroguardia95@gmail.com>
2022-05-28 23:30:34 -0300

    BaSP-TG-36:Add-Edit-Projects

--- src/Components/Projects/index.js
+++ src/Components/Projects/index.js
@@ -1,9 +1,35 @@
+import { useEffect, useState } from 'react';
 import styles from './projects.module.css';
 
 function Projects() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/projects`)
+      .then((response) => response.json())
+      .then((response) => {
+        setProjects(response.data);
+      });
+  }, []);
+
   return (
     <section className={styles.container}>
-      <h2>Projects</h2>
+      <h2>Project</h2>
+      <div>
+        {projects.map((project) => {
+          return (
+            <div
+              key={project.name}
+              isActive={project.isActive}
+              description={project.description}
+              client={project.client}
+              startDate={project.startDate}
+              endDate={project.endDate}
+              members={project.members}
+            ></div>
+          );
+        })}
+      </div>
     </section>
   );
 }
