e8eec1746fc4e8ad9137ebb733058a746def8673
Alvaro-Guardia <alvaroguardia95@gmail.com>
2022-05-28 22:12:26 -0300

    BaSP-TG-36:Add-Edit-Projects

--- src/Components/Admins/index.js
+++ src/Components/Admins/index.js
@@ -1,9 +1,35 @@
+import { useEffect, useState } from 'react';
 import styles from './admins.module.css';
 
 function Admins() {
+  const [admins, setAdmins] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/admins`)
+      .then((response) => response.json())
+      .then((response) => {
+        setAdmins(response.data);
+      });
+  }, []);
+
   return (
     <section className={styles.container}>
       <h2>Admins</h2>
+      <div>
+        {admins.map((admin) => {
+          return (
+            <div
+              key={admin.id}
+              firstName={admin.firstName}
+              lastName={admin.lastName}
+              email={admin.email}
+              gender={admin.gender}
+              active={admin.active}
+              paswword={admin.password}
+            ></div>
+          );
+        })}
+      </div>
     </section>
   );
 }
