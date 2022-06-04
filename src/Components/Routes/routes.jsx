3abd3f6015663526367f62a58941995a18d0cd8d
Ivan <gobbi.ivandog@gmail.com>
2022-06-04 11:47:10 -0300

    BaSP-TG-49: home path added

--- src/Components/Routes/routes.jsx
+++ src/Components/Routes/routes.jsx
@@ -7,10 +7,12 @@ import Employees from '../Employees/index';
 import Projects from '../Projects';
 import TimeSheets from '../TimeSheets/index';
 import Tasks from '../Tasks/index';
+import Home from '../Home';
 
 const Routes = () => {
   return (
     <Switch>
+      <Route path="/" exact component={Home} />
       <Route path="/super-admins" exact component={SuperAdmins} />
       <Route path="/super-admins/form" exact component={SuperAdminsForm} />
       <Route path="/admins" exact component={Admins} />
