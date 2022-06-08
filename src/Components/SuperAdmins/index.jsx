405d4886ded24b32e1e2740028766e9e00e640c8
Ivan <gobbi.ivandog@gmail.com>
2022-06-07 23:54:58 -0300

    BaSP-TG-57: buttons and delete function implementation

--- src/Components/SuperAdmins/index.jsx
+++ src/Components/SuperAdmins/index.jsx
@@ -3,11 +3,14 @@ import React, { useState, useEffect } from 'react';
 import styles from './super-admins.module.css';
 import Button from '../Shared/Buttons/buttons';
 import Table from '../Shared/Table/Table';
-// import Modal from '../Shared/Modal/Modal';
+import Modal from '../Shared/Modal/Modal';
 
 function SuperAdmins() {
   const [superAdmins, setSuperAdmins] = useState([]);
   const url = `${process.env.REACT_APP_API_URL}/super-admins`;
+  const [id, setId] = useState('');
+  const [isOpen, setIsOpen] = useState(false);
+
   useEffect(() => {
     try {
       fetch(url)
@@ -19,9 +22,46 @@ function SuperAdmins() {
       console.error(error);
     }
   }, []);
-  // const [isOpen, setIsOpen] = useState(false);
 
-  const headers = ['_id', 'firstName', 'lastName', 'email', 'password'];
+  const getData = () => {
+    return superAdmins.map((superAdmin) => ({
+      ...superAdmin,
+      edit: (
+        <Button
+          icons="edit"
+          // callback={() => {
+          //   onEdit(superAdmin);
+          // }}
+        />
+      ),
+      delete: (
+        <Button
+          icons="delete"
+          callback={() => {
+            setIsOpen(true);
+            setId(superAdmin._id);
+          }}
+        />
+      )
+    }));
+  };
+
+  const deleteRow = async (_id) => {
+    const resp = await fetch(`${url}/${_id}`, {
+      method: 'DELETE'
+    });
+    const data = resp.json;
+    console.log(data);
+    console.log(resp);
+    if (resp.status === 200) {
+      setSuperAdmins(superAdmins.filter((row) => row._id !== _id));
+      setIsOpen(false);
+    } else {
+      alert('There has been an error');
+    }
+  };
+
+  const headers = ['_id', 'firstName', 'lastName', 'email', 'password', 'edit', 'delete'];
   return (
     <section className={styles.container}>
       <div>
@@ -31,16 +71,11 @@ function SuperAdmins() {
           {/* <Button icons={'add'} onClick={setIsOpen(true)} /> */}
         </a>
         {/* <List /> */}
-        <Table
-          data={superAdmins}
-          headers={headers}
-          actions={
-            <>
-              <Button icons={'edit'} />
-              <Button icons={'delete'} />
-            </>
-          }
-        />
+        <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
+          <h1>modal test</h1>
+          <Button callback={() => deleteRow(id)} text={'Delete'} />
+        </Modal>
+        <Table data={getData()} headers={headers} />
       </div>
     </section>
   );
