9e87317fcf34bb2a3e1e3418e604ed0060b4f591
Ivan <gobbi.ivandog@gmail.com>
2022-06-11 17:47:02 -0300

    BaSP-TG-70: Initial setup redux ,testing

---
+++ src/redux/store.js
@@ -0,0 +1,14 @@
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+
+const rootReducer = combineReducers({});
+
+const configureStore = () => {
+  const enhancer = composeWithDevTools(applyMiddleware(thunk));
+  return createStore(rootReducer, enhancer);
+};
+
+const store = configureStore();
+
+export default store;
