e9238d024cc843a4ab5e265bda6e22d3af65da01
Axel <axelbaco@hotmail.com>
2022-06-16 19:33:53 -0300

    BaSP-TG-64: Fixed Modal button

--- src/Components/Shared/Modal/index.jsx
+++ src/Components/Shared/Modal/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import styles from './modal.module.css';
 import Button from '../Buttons/buttons';
-
 const Modal = ({ children, isOpen, setIsOpen, title }) => {
   if (!isOpen) {
     return null;
