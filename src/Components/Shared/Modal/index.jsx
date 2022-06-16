e5b3306dece3b645e8ad955a69911f760fdc36f4
Alvaro-Guardia <alvaroguardia95@gmail.com>
2022-06-16 14:42:44 -0300

    BaSP-TG-79: Modal change

--- src/Components/Shared/Modal/index.jsx
+++ src/Components/Shared/Modal/index.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import styles from './modal.module.css';
 import Button from '../Buttons/buttons';
 
-const Modal = ({ children, isOpen, setIsOpen, message }) => {
+const Modal = ({ children, isOpen, setIsOpen, title }) => {
   if (!isOpen) {
     return null;
   }
   return (
     <div className={styles.overlay}>
       <div className={styles.contenedorModal}>
-        <h3>{message}</h3>
+        <h3>{title}</h3>
         <Button
           className={styles.buttonCloseModal}
           callback={() => setIsOpen(false)}
