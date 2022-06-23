1c3d96b3e82b7280499c6eabf099cb8a0b3beaf6
LautaroCaballero <86745664+LautaroCaballero@users.noreply.github.com>
2022-06-23 14:34:49 -0300

    BaSP-TG-90: React hook forms added

--- src/Components/Admins/AddModal.jsx
+++ src/Components/Admins/AddModal.jsx
@@ -1,97 +1,119 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Modal from '../Shared/Modal/index';
-import Input from '../Shared/Input';
 import Button from '../Shared/Buttons/buttons';
-import Dropdown from '../Shared/Dropdown/Dropdown';
+import DropdownForm from '../Shared/dropdownForm/dropdownForm';
 import styles from './admins.module.css';
+import InputControlled from '../Shared/InputControlled';
+import Joi, { required } from 'joi';
+import { joiResolver } from '@hookform/resolvers/joi';
 import { createAdmin } from '../../redux/admins/thunks';
 import { useDispatch } from 'react-redux';
+import { useForm } from 'react-hook-form';
 
 const ModalAddAdmin = ({ setShowAddModal, showAddModal, setSucModalIsOpen }) => {
-  const [name, setName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [email, setEmail] = useState('');
-  const [gender, setGender] = useState('');
-  const [status, setStatus] = useState('');
-  const [password, setPassword] = useState('');
-
   const dispatch = useDispatch();
 
+  const adminSchema = Joi.object({
+    name: Joi.string().required().min(3),
+    lastName: Joi.string().required().min(3),
+    email: Joi.string().required(),
+    password: Joi.string().required().min(8),
+    gender: Joi.string().required(),
+    status: Joi.string().required()
+  });
+
+  const {
+    handleSubmit,
+    register,
+    reset,
+    setValue,
+    formState: { errors }
+  } = useForm({
+    mode: 'onSubmit',
+    resolver: joiResolver(adminSchema)
+  });
+
+  const addAdminHandler = ({ name, lastName, email, gender, status, password }, e) => {
+    e.preventDefault();
+    dispatch(createAdmin(name, lastName, email, gender, status, password));
+    setShowAddModal(false);
+    setSucModalIsOpen(true);
+    reset();
+  };
+
+  useEffect(() => {
+    reset();
+  }, []);
+
+  // const addAdmin = ({}, e) => {
+  //   e.preventDefault();
+  // }
   return (
     <Modal isOpen={showAddModal} setIsOpen={setShowAddModal}>
-      <div className={styles.addModalContainer}>
-        <div>
-          <Input
-            labelText={'Name'}
-            type="text"
-            placeholder="Name"
-            onChange={(e) => {
-              setName(e.target.value);
-            }}
-          />
-          <Input
-            labelText={'Last Name'}
-            type="text"
-            placeholder="Last Name"
-            onChange={(e) => {
-              setLastName(e.target.value);
-            }}
-          />
-          <Input
-            labelText={'Email'}
-            type={'text'}
-            placeholder={'Email'}
-            onChange={(e) => {
-              setEmail(e.target.value);
-            }}
-          />
+      <form onSubmit={handleSubmit(addAdminHandler)}>
+        <div className={styles.addModalContainer}>
+          <div>
+            <InputControlled
+              label={'Name'}
+              type={'text'}
+              name={'name'}
+              placeholder="Name"
+              register={register}
+              required
+              error={errors.name}
+            />
+            <InputControlled
+              label={'Last Name'}
+              type={'text'}
+              name={'lastName'}
+              placeholder="Last Name"
+              register={register}
+              required
+              error={errors.lastName}
+            />
+            <InputControlled
+              label={'Email'}
+              type={'text'}
+              name={'email'}
+              placeholder="Email"
+              register={register}
+              required
+              error={errors.email}
+            />
+          </div>
+          <div>
+            <DropdownForm
+              label="Gender"
+              options={['male', 'female', 'polygender']}
+              name="gender"
+              initialOption="Select a gender"
+              required
+              register={register}
+              error={errors.gender}
+            />
+            <DropdownForm
+              label="Status"
+              options={['true', 'false']}
+              name={'status'}
+              required
+              initialOption="Select a status"
+              register={register}
+              error={errors.status}
+            />
+            <InputControlled
+              label={'Password'}
+              type={'password'}
+              name={'password'}
+              placeholder="Password"
+              register={register}
+              required
+              error={errors.password}
+            />
+          </div>
         </div>
-        <div>
-          <Dropdown
-            label="Gender"
-            options={['male', 'female', 'polygender']}
-            value={gender}
-            initialOption="Select a gender"
-            onChange={(e) => {
-              setGender(e.target.value);
-            }}
-          />
-          <Dropdown
-            label="Status"
-            options={['true', 'false']}
-            value={status}
-            initialOption="Select a status"
-            onChange={(e) => {
-              setStatus(e.target.value);
-            }}
-          />
-          <Input
-            labelText={'Password'}
-            type="password"
-            placeholder="password"
-            onChange={(e) => {
-              setPassword(e.target.value);
-            }}
-          />
-        </div>
-      </div>
-      <Button
-        value="Submit"
-        icons={'submit'}
-        callback={() => {
-          createAdmin(
-            name,
-            lastName,
-            email,
-            gender,
-            status,
-            password,
-            setShowAddModal,
-            setSucModalIsOpen
-          )(dispatch);
-        }}
-      />
+        <Button value="Submit" icons={'submit'} />
+      </form>
     </Modal>
   );
 };
