506a3f0bbc42c0d1b53131813c0b566ebc296eaa
Matías Cellamare <88092713+cellamarematias@users.noreply.github.com>
2022-06-25 16:08:15 -0300

    BaSP-TG-102: added home site

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,9 +1,18 @@
 import styles from './home.module.css';
+import clock from './Assets/clock.jpg';
 
 function Home() {
   return (
     <section className={styles.container}>
-      <h2>Home</h2>
+      <h2 className={styles.tittle}>Welcome to Trackgenix!</h2>
+      <div className={styles.homeContent}>
+        <h3 className={styles.homeh3}>A free website for everyone at Trackgenix!</h3>
+        <img src={clock} alt="clock img" />
+      </div>
+      <p className={styles.homeP}>
+        Here, you can register the hours worked in any project for all employees, generate reports
+        and much more.
+      </p>
     </section>
   );
 }
