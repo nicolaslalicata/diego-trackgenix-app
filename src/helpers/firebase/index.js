6829be6b9b27fe74adf2dd88e8b0cbb217dd0cd5
Agustin <alchazarreta0@gmail.com>
2022-07-13 22:12:51 -0300

    helper change test api key

--- src/helpers/firebase/index.js
+++ src/helpers/firebase/index.js
@@ -4,7 +4,7 @@ import { initializeApp } from 'firebase/app';
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
+  apiKey: 'AIzaSyB92vtjP7thXuG3_ZVAEVMp0BF5Py5KZG8',
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
